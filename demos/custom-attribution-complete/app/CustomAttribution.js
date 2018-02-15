57cd75df65618c581521a64c3b67270d5e1a1438
Matt Driscoll <mdriscoll@esri.com>
2018-02-15 14:22:18 -0800

    ready to start coding

--- demos/custom-attribution-complete/app/CustomAttribution.js
+++ demos/custom-attribution-complete/app/CustomAttribution.js
@@ -19,11 +19,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 define(["require", "exports", "esri/core/tsSupport/declareExtendsHelper", "esri/core/tsSupport/decorateHelper", "esri/core/accessorSupport/decorators", "esri/widgets/support/widget", "esri/widgets/Widget", "esri/widgets/Attribution/AttributionViewModel", "esri/core/watchUtils"], function (require, exports, __extends, __decorate, decorators_1, widget_1, Widget, AttributionViewModel, watchUtils) {
     "use strict";
     var CSS = {
-        base: "esri-attribution esri-widget",
+        base: "esri-custom-attribution esri-widget",
         poweredBy: "esri-attribution__powered-by",
         sources: "esri-attribution__sources",
-        open: "esri-attribution--open",
-        sourcesOpen: "esri-attribution__sources--open",
         link: "esri-attribution__link",
         // common.css
         interactive: "esri-interactive"
@@ -35,60 +33,19 @@ define(["require", "exports", "esri/core/tsSupport/declareExtendsHelper", "esri/
         //  Lifecycle
         //
         //--------------------------------------------------------------------------
-        /**
-         * @constructor
-         * @alias module:esri/widgets/Attribution
-         * @extends module:esri/widgets/Widget
-         * @param {Object} [properties] - See the [properties](#properties-summary) for a list of all the properties
-         *                                that may be passed into the constructor.
-         */
         function Attribution(params) {
             var _this = _super.call(this) || this;
-            //--------------------------------------------------------------------------
-            //
-            //  Variables
-            //
-            //--------------------------------------------------------------------------
-            _this._isOpen = false;
-            _this._attributionTextOverflowed = false;
-            _this._prevSourceNodeHeight = 0;
             //----------------------------------
             //  itemDelimiter
             //----------------------------------
-            /**
-             * Text used to split attribution by {@link module:esri/layers/Layer layers}
-             *
-             * @name itemDelimiter
-             * @instance
-             * @type {string}
-             * @default |
-             */
             _this.itemDelimiter = " | ";
             //----------------------------------
             //  view
             //----------------------------------
-            /**
-             * A reference to the {@link module:esri/views/MapView} or {@link module:esri/views/SceneView}. Set this to link the widget to a specific view.
-             *
-             * @type {module:esri/views/MapView | module:esri/views/SceneView}
-             * @name view
-             * @instance
-             */
             _this.view = null;
             //----------------------------------
             //  viewModel
             //----------------------------------
-            /**
-             * The view model for this widget. This is a class that contains all the logic
-             * (properties and methods) that controls this widget's behavior. See the
-             * {@link module:esri/widgets/Attribution/AttributionViewModel} class to access
-             * all properties and methods on the widget.
-             *
-             * @name viewModel
-             * @instance
-             * @autocast
-             * @type {module:esri/widgets/Attribution/AttributionViewModel}
-             */
             _this.viewModel = new AttributionViewModel();
             return _this;
         }
@@ -99,20 +56,17 @@ define(["require", "exports", "esri/core/tsSupport/declareExtendsHelper", "esri/
         Object.defineProperty(Attribution.prototype, "attributionText", {
             //--------------------------------------------------------------------------
             //
+            //  Variables
+            //
+            //--------------------------------------------------------------------------
+            //--------------------------------------------------------------------------
+            //
             //  Properties
             //
             //--------------------------------------------------------------------------
             //----------------------------------
             //  attributionText
             //----------------------------------
-            /**
-             * Full attribution text.
-             *
-             * @name attributionText
-             * @instance
-             * @type {string}
-             * @readonly
-             */
             get: function () {
                 return this.viewModel.items.map(function (item) { return item.text; }).join(this.itemDelimiter);
             },
@@ -125,65 +79,7 @@ define(["require", "exports", "esri/core/tsSupport/declareExtendsHelper", "esri/
         //
         //--------------------------------------------------------------------------
         Attribution.prototype.render = function () {
-            var classes = (_a = {},
-                _a[CSS.open] = this._isOpen,
-                _a);
-            return (widget_1.tsx("div", { bind: this, class: CSS.base, classes: classes, onclick: this._toggleState, onkeydown: this._toggleState },
-                this._renderSourcesNode(),
-                widget_1.tsx("div", { class: CSS.poweredBy },
-                    "Powered by ",
-                    widget_1.tsx("a", { target: "_blank", href: "http://www.esri.com/", class: CSS.link }, "Esri"))));
-            var _a;
-        };
-        //--------------------------------------------------------------------------
-        //
-        //  Private Methods
-        //
-        //--------------------------------------------------------------------------
-        Attribution.prototype._renderSourcesNode = function () {
-            var isOpen = this._isOpen;
-            var interactive = this._isInteractive();
-            var sourceTabIndex = interactive ? 0 : -1;
-            var attributionText = this.attributionText;
-            var role = interactive ? "button" : undefined;
-            var sourceClasses = (_a = {},
-                _a[CSS.sourcesOpen] = isOpen,
-                _a[CSS.interactive] = interactive,
-                _a);
-            return widget_1.tsx("div", { afterCreate: this._afterSourcesNodeCreate, afterUpdate: this._afterSourcesNodeUpdate, bind: this, class: CSS.sources, classes: sourceClasses, innerHTML: attributionText, role: role, tabIndex: sourceTabIndex });
-            var _a;
-        };
-        Attribution.prototype._afterSourcesNodeCreate = function (element) {
-            this._prevSourceNodeHeight = element.clientWidth;
-        };
-        Attribution.prototype._afterSourcesNodeUpdate = function (element) {
-            var shouldRender = false;
-            var clientHeight = element.clientHeight, clientWidth = element.clientWidth, scrollWidth = element.scrollWidth;
-            var attributionTextOverflowed = scrollWidth >= clientWidth;
-            var attributionTextOverflowChanged = this._attributionTextOverflowed !== attributionTextOverflowed;
-            this._attributionTextOverflowed = attributionTextOverflowed;
-            if (attributionTextOverflowChanged) {
-                shouldRender = true;
-            }
-            if (this._isOpen) {
-                var recentlyClosed = clientHeight < this._prevSourceNodeHeight;
-                this._prevSourceNodeHeight = clientHeight;
-                if (recentlyClosed) {
-                    this._isOpen = false;
-                    shouldRender = true;
-                }
-            }
-            if (shouldRender) {
-                this.scheduleRender();
-            }
-        };
-        Attribution.prototype._toggleState = function () {
-            if (this._isInteractive()) {
-                this._isOpen = !this._isOpen;
-            }
-        };
-        Attribution.prototype._isInteractive = function () {
-            return this._isOpen || this._attributionTextOverflowed;
+            return (widget_1.tsx("div", { class: CSS.base }, "Hello world"));
         };
         __decorate([
             decorators_1.property({
@@ -208,9 +104,6 @@ define(["require", "exports", "esri/core/tsSupport/declareExtendsHelper", "esri/
                 "view.size"
             ])
         ], Attribution.prototype, "viewModel", void 0);
-        __decorate([
-            widget_1.accessibleHandler()
-        ], Attribution.prototype, "_toggleState", null);
         Attribution = __decorate([
             decorators_1.subclass("esri.widgets.Attribution")
         ], Attribution);
--- demos/custom-attribution-complete/app/CustomAttribution.tsx
+++ demos/custom-attribution-complete/app/CustomAttribution.tsx
@@ -13,11 +13,9 @@ import watchUtils = require("esri/core/watchUtils");
 import View = require("esri/views/View");
 
 const CSS = {
-  base: "esri-attribution esri-widget",
+  base: "esri-custom-attribution esri-widget",
   poweredBy: "esri-attribution__powered-by",
   sources: "esri-attribution__sources",
-  open: "esri-attribution--open",
-  sourcesOpen: "esri-attribution__sources--open",
   link: "esri-attribution__link",
 
   // common.css
@@ -33,13 +31,6 @@ class Attribution extends declared(Widget) {
   //
   //--------------------------------------------------------------------------
 
-  /**
-   * @constructor
-   * @alias module:esri/widgets/Attribution
-   * @extends module:esri/widgets/Widget
-   * @param {Object} [properties] - See the [properties](#properties-summary) for a list of all the properties
-   *                                that may be passed into the constructor.
-   */
   constructor(params?: any) {
     super();
   }
@@ -56,11 +47,6 @@ class Attribution extends declared(Widget) {
   //
   //--------------------------------------------------------------------------
 
-  private _isOpen = false;
-
-  private _attributionTextOverflowed = false;
-
-  private _prevSourceNodeHeight = 0;
 
   //--------------------------------------------------------------------------
   //
@@ -72,14 +58,6 @@ class Attribution extends declared(Widget) {
   //  attributionText
   //----------------------------------
 
-  /**
-   * Full attribution text.
-   *
-   * @name attributionText
-   * @instance
-   * @type {string}
-   * @readonly
-   */
   @property({
     dependsOn: ["viewModel.items.length", "itemDelimiter"],
     readOnly: true
@@ -93,14 +71,6 @@ class Attribution extends declared(Widget) {
   //  itemDelimiter
   //----------------------------------
 
-  /**
-   * Text used to split attribution by {@link module:esri/layers/Layer layers}
-   *
-   * @name itemDelimiter
-   * @instance
-   * @type {string}
-   * @default |
-   */
   @property()
   @renderable()
   itemDelimiter = " | ";
@@ -109,13 +79,6 @@ class Attribution extends declared(Widget) {
   //  view
   //----------------------------------
 
-  /**
-   * A reference to the {@link module:esri/views/MapView} or {@link module:esri/views/SceneView}. Set this to link the widget to a specific view.
-   *
-   * @type {module:esri/views/MapView | module:esri/views/SceneView}
-   * @name view
-   * @instance
-   */
   @aliasOf("viewModel.view")
   view: View = null;
 
@@ -123,17 +86,6 @@ class Attribution extends declared(Widget) {
   //  viewModel
   //----------------------------------
 
-  /**
-   * The view model for this widget. This is a class that contains all the logic
-   * (properties and methods) that controls this widget's behavior. See the
-   * {@link module:esri/widgets/Attribution/AttributionViewModel} class to access
-   * all properties and methods on the widget.
-   *
-   * @name viewModel
-   * @instance
-   * @autocast
-   * @type {module:esri/widgets/Attribution/AttributionViewModel}
-   */
   @property({
     type: AttributionViewModel
   })
@@ -150,20 +102,10 @@ class Attribution extends declared(Widget) {
   //--------------------------------------------------------------------------
 
   render() {
-    const classes = {
-      [CSS.open]: this._isOpen
-    };
-
     return (
-      <div bind={this}
-        class={CSS.base}
-        classes={classes}
-        onclick={this._toggleState}
-        onkeydown={this._toggleState}>
-        {this._renderSourcesNode()}
-        <div class={CSS.poweredBy}>Powered by <a target="_blank"
-          href="http://www.esri.com/"
-          class={CSS.link}>Esri</a></div>
+      <div
+        class={CSS.base}>
+        Hello world
       </div>
     );
   }
@@ -174,70 +116,6 @@ class Attribution extends declared(Widget) {
   //
   //--------------------------------------------------------------------------
 
-  private _renderSourcesNode(): any {
-    const isOpen = this._isOpen;
-    const interactive = this._isInteractive();
-    const sourceTabIndex = interactive ? 0 : -1;
-    const { attributionText } = this;
-    const role = interactive ? "button" : undefined;
-
-    const sourceClasses = {
-      [CSS.sourcesOpen]: isOpen,
-      [CSS.interactive]: interactive
-    };
-
-    return <div afterCreate={this._afterSourcesNodeCreate}
-      afterUpdate={this._afterSourcesNodeUpdate}
-      bind={this}
-      class={CSS.sources}
-      classes={sourceClasses}
-      innerHTML={attributionText}
-      role={role}
-      tabIndex={sourceTabIndex} />;
-  }
-
-  private _afterSourcesNodeCreate(element: Element): void {
-    this._prevSourceNodeHeight = element.clientWidth;
-  }
-
-  private _afterSourcesNodeUpdate(element: Element): void {
-    let shouldRender = false;
-
-    const { clientHeight, clientWidth, scrollWidth } = element;
-
-    const attributionTextOverflowed = scrollWidth >= clientWidth;
-    const attributionTextOverflowChanged = this._attributionTextOverflowed !== attributionTextOverflowed;
-    this._attributionTextOverflowed = attributionTextOverflowed;
-
-    if (attributionTextOverflowChanged) {
-      shouldRender = true;
-    }
-
-    if (this._isOpen) {
-      const recentlyClosed = clientHeight < this._prevSourceNodeHeight;
-      this._prevSourceNodeHeight = clientHeight;
-
-      if (recentlyClosed) {
-        this._isOpen = false;
-        shouldRender = true;
-      }
-    }
-
-    if (shouldRender) {
-      this.scheduleRender();
-    }
-  }
-
-  @accessibleHandler()
-  private _toggleState(): void {
-    if (this._isInteractive()) {
-      this._isOpen = !this._isOpen;
-    }
-  }
-
-  private _isInteractive(): boolean {
-    return this._isOpen || this._attributionTextOverflowed;
-  }
 
 }
 

ce2d11a29eba8dbeb42a7319e0c03c0c1190ef9d
Matt Driscoll <mdriscoll@esri.com>
2018-03-02 14:21:32 -0800

    nls, cleanup

---
+++ demos/custom-attribution-complete/app/nls/es/AttributionTable.js
@@ -0,0 +1,9 @@
+define({
+  widgetLabel: "Tabla de atribución",
+  noAttribution: "Sin atribución para mostrar",
+  fullExtent: "Ver la extensión completa de la capa",
+  externalLink: "Ver la URL del servicio de la capa",
+  columnTitle: "Título",
+  columnType: "Tipo  ",
+  columnSources: "Fuente(s)"
+});

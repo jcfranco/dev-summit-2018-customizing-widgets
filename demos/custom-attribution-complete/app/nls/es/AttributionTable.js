1f9ac25b6139ea99bf22342b692d071441768c83
JC Franco <jfranco@esri.com>
2018-03-03 15:52:47 -0800

    Tweak translations.

--- demos/custom-attribution-complete/app/nls/es/AttributionTable.js
+++ demos/custom-attribution-complete/app/nls/es/AttributionTable.js
@@ -1,9 +1,9 @@
 define({
   widgetLabel: "Tabla de atribución",
   noAttribution: "Sin atribución para mostrar",
-  fullExtent: "Ver la extensión completa de la capa",
-  externalLink: "Ver la URL del servicio de la capa",
+  fullExtent: "Ver extensión completa de la capa",
+  externalLink: "Ver dirección URL del servicio de la capa",
   columnTitle: "Título",
-  columnType: "Tipo  ",
+  columnType: "Tipo",
   columnSources: "Fuente(s)"
 });
